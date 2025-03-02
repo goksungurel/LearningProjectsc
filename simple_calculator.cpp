91cfd8280ffad7bf2ef6a4f4d7346f5bd5ee3835
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-03-02 19:44:33 +0300

    simple_calculator.cpp

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -6,43 +6,42 @@ int main() {
     int choice;
 
     while (true) {
-        cout << "Birinci sayıyı girin: ";
+        cout << "Enter the first number: ";
         cin >> num1;
-        cout << "İkinci sayıyı girin: ";
+        cout << "Enter the second number: ";
         cin >> num2;
       
-        cout << "\nİşlem Seçin:" << endl;
-        cout << "1. Toplama" << endl;
-        cout << "2. Çıkarma" << endl;
-        cout << "3. Çarpma" << endl;
-        cout << "4. Bölme" << endl;
-        cout << "5. Çıkış" << endl;
-        cout << "Seçiminizi yapın (1/2/3/4/5): ";
+        cout << "\nChoose an operation:" << endl;
+        cout << "1. Addition" << endl;
+        cout << "2. Subtraction" << endl;
+        cout << "3. Multiplication" << endl;
+        cout << "4. Division" << endl;
+        cout << "5. Exit" << endl;
+        cout << "Make your choice (1/2/3/4/5): ";
         cin >> choice;
 
-      
         switch (choice) {
             case 1:
-                cout << "Sonuç: " << num1 + num2 << endl;
+                cout << "Result: " << num1 + num2 << endl;
                 break;
             case 2:
-                cout << "Sonuç: " << num1 - num2 << endl;
+                cout << "Result: " << num1 - num2 << endl;
                 break;
             case 3:
-                cout << "Sonuç: " << num1 * num2 << endl;
+                cout << "Result: " << num1 * num2 << endl;
                 break;
             case 4:
                 if (num2 != 0) {
-                    cout << "Sonuç: " << num1 / num2 << endl;
+                    cout << "Result: " << num1 / num2 << endl;
                 } else {
-                    cout << "Bölme işlemi sıfıra yapılamaz!" << endl;
+                    cout << "Division by zero is not allowed!" << endl;
                 }
                 break;
             case 5:
-                cout << "Çıkılıyor... Görüşürüz!" << endl;
+                cout << "Exiting... Goodbye!" << endl;
                 return 0;
             default:
-                cout << "Geçersiz giriş, lütfen tekrar deneyin." << endl;
+                cout << "Invalid input, please try again." << endl;
         }
         cout << "\n";
     }
